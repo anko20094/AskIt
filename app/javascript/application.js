827b5a20548d2bb4720dd9ace0823392b1fbd679
Danyil Shkoropad <31440971+anko20094@users.noreply.github.com>
2022-10-30 00:06:45 +0300

    Migrate to ESbuild and DartSass and yarn v3. Delete webpacker (#16)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,7 @@ import * as ActiveStorage from "@rails/activestorage"
 
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
-import './scripts/select2'
+import './scripts/select'
 
 Rails.start()
 Turbolinks.start()
