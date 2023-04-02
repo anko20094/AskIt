95209461a037eaefc5454d9ba95a2f85a7a06abb
Danyil Shkoropad <31440971+anko20094@users.noreply.github.com>
2023-04-02 14:30:20 +0300

    Upgrade gems and fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
-
+#
 User.create(name: 'Denko', email: 'anko20094@gmail.com', password: 'Danko1996@',
-            password_digest: 'Danko1996@')
+            password_digest: '$2a$12$wQwxHJVALGRWfjnQoyngOuYrxWKNCRSQ1MmCoxqO9tyonDnFY9IFS',
+            gravatar_hash: 'c309006b90a81da636d2bc8b31201547')
 
 30.times do
   Question.create(title: Faker::Hipster.sentence(word_count: 3),
                   body: Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4),
-                  user_id: 1)
+                  user_id: 2)
 end
 
 User.find_each do |u|
